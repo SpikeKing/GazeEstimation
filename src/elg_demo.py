f14382b211659ba6378ec5a29d32837d79bbaa79
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-04-09 21:00:02 +0800

    update

--- src/elg_demo.py
+++ src/elg_demo.py
@@ -233,15 +233,15 @@ if __name__ == '__main__':
                     u0 = 0 if eye_side == 'left' else ew
                     u1 = u0 + ew
 
-                    print('[Info] bgr: {}'.format(bgr.shape))
-                    print('[Info] bgr[v0:v1, u0:u1]: {}, u0: {}, u1: {}'.format(bgr[v0:v1, u0:u1].shape, u0, u1))
-                    print('[Info] eye_image_raw: {}'.format(eye_image_raw.shape))
-
                     bgr_h, bgr_w, _ = bgr.shape
                     if u1 > bgr_w:
                         u1 = bgr_w
 
-                    bgr[v0:v1, u0:u1] = eye_image_raw
+                    print('[Info] bgr: {}'.format(bgr.shape))
+                    print('[Info] bgr[v0:v1, u0:u1]: {}, u0: {}, u1: {}'.format(bgr[v0:v1, u0:u1].shape, u0, u1))
+                    print('[Info] eye_image_raw: {}'.format(eye_image_raw.shape))
+
+                    bgr[v0:v1, u0:u1] = eye_image_raw[:, 0:u1 - u0]
                     bgr[v1:v2, u0:u1] = eye_image_annotated
 
                     # Visualize preprocessing results
